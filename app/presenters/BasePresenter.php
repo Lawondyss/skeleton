b95ac67ccd0b185a7fd8fc6c162cf10f88823d96
Ladislav Vondracek <lawondyss@gmail.com>
2014-08-02 20:57:14 +0200

    Added WebLoader

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -3,9 +3,14 @@
 namespace App\Presenters;
 
 use Nette\Application\UI\Presenter;
+use WebLoader\Nette\CssLoader;
 
 abstract class BasePresenter extends Presenter
 {
+  /** @var \WebLoader\Nette\LoaderFactory @inject */
+  public $webloader;
+
+
   protected function startup()
   {
     parent::startup();
@@ -17,4 +22,38 @@ abstract class BasePresenter extends Presenter
     parent::beforeRender();
   }
 
+
+  /**
+   * @return \WebLoader\Nette\CssLoader
+   */
+  protected function createComponentCssScreen()
+  {
+    $control = $this->webloader->createCssLoader('screen');
+    $control->setMedia('screen,projection,tv');
+
+    return $control;
+  }
+
+
+  /**
+   * @return \WebLoader\Nette\CssLoader
+   */
+  protected function createComponentCssPrint()
+  {
+    $control = $this->webloader->createCssLoader('print');
+    $control->setMedia('print');
+
+    return $control;
+  }
+
+
+  /**
+   * @return \WebLoader\Nette\JavaScriptLoader
+   */
+  protected function createComponentJs()
+  {
+    $control = $this->webloader->createJavaScriptLoader('default');
+
+    return $control;
+  }
 }
