7cde33572e5aaeee2e145f0ff2da75b6796af97f
Ladislav Vondracek <lawondyss@gmail.com>
2014-08-26 11:22:22 +0200

    Added methods for password and token

--- app/security/Authenticator.php
+++ app/security/Authenticator.php
@@ -49,4 +49,50 @@ class Authenticator extends \Nette\Object implements NS\IAuthenticator
 
     return $identity;
   }
+
+
+  /**
+   * @param string $password
+   * @return string
+   */
+  public static function hashPassword($password)
+  {
+    $hash = NS\Passwords::hash($password);
+    return $hash;
+  }
+
+
+  /**
+   * @param string $password
+   * @param string $hash
+   * @return bool
+   */
+  public static function verifyPassword($password, $hash)
+  {
+    $verify = NS\Passwords::verify($password, $hash);
+    return $verify;
+  }
+
+
+  /**
+   * @return string
+   */
+  public static function generateToken()
+  {
+    $token = (string)(microtime(true) * 10000);
+    return $token;
+  }
+
+
+  /**
+   * @param string|int $token
+   * @param int $timeLimit
+   * @return bool
+   */
+  public static function validateToken($token, $timeLimit)
+  {
+    $expireTime = (int)$token / 10000 + $timeLimit;
+    $validity = $expireTime < time();
+    return $validity;
+  }
 }
